61b443feabf3e8ce9a3970d6713036dafcef4363
mikej <mikej@sunrise.(none)>
2014-06-03 10:43:21 +0800

    optimized

--- v2ex_get_coin.php
+++ v2ex_get_coin.php
@@ -1,19 +1,17 @@
 <?php
-define(LOGIN_URL, "http://v2ex.com/signin");
-define(COIN_URL, "http://v2ex.com/mission/daily");
-define(GET_COIN_URL, "http://v2ex.com/mission/daily/redeem?once=");
-define(USER_AGENT, "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0");
-define(COOKIE_FILE, dirname(__file__)."/v2ex.cookie");
-define(U, "username");
-define(P, "password");
+define("LOGIN_URL", "http://v2ex.com/signin");
+define("COIN_URL", "http://v2ex.com/mission/daily");
+define("GET_COIN_URL", "http://v2ex.com/mission/daily/redeem?once=");
+define("USER_AGENT", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0");
+define("COOKIE_FILE", dirname(__file__)."/v2ex.cookie");
+define("U", "username");
+define("P", "password");
 
 $ip = array(
 	'CLIENT-IP: '.randIp(),
 	'X-FORWARDED-FOR: '.randIp()
 );
-
-$error = "";
-$error_num = 0;
+$error = 0;
 
 function randIp(){
 	return rand(60, 255).'.'.rand(60, 255).'.'.rand(60, 255).'.'.rand(60, 255);
@@ -23,7 +21,7 @@ function getLoginCode($data){
 	if(preg_match("/value=\"(\d{5})\"\sname=\"once\"/", $data, $matches)){
 		return $matches[1];
 	}else{
-		return false;
+		log("login_code error");
 	}
 }
 
@@ -31,8 +29,14 @@ function getCoinCode($data){
 	if(preg_match("/\'\/mission\/daily\/redeem\?once=(\d{5})\'\;/", $data, $matches)){
 		return $matches[1];
 	}else{
-		return false;
+		log("coin_code error");
 	}
+}
+
+function log($data){
+	$data = date("Y-m-d")."\t".$data."\r\n";
+	file_put_contents("error_log", $data, FILE_APPEND);
+	exit;
 }
 
 function send($url, $post_data = false, $referer = ""){
@@ -52,11 +56,9 @@ function send($url, $post_data = false, $referer = ""){
 	curl_setopt($ch, CURLOPT_COOKIEJAR, COOKIE_FILE);
 	$data = curl_exec($ch);
 	curl_close($ch);
-	$error_num++;
+	$GLOBALS['error']++;
 	if($data === false){
-		$error .= "error".$error_num."\t".curl_error($ch)."\t".date("Y-m-d")."\r\n";
-		file_put_contents("error_log", $error, FILE_APPEND);
-		exit;
+		log("error{$GLOBALS['error']}:".curl_error($ch));
 	}else{
 		return $data;
 	}
@@ -72,4 +74,9 @@ $coin_html = send(COIN_URL);
 $coin_code = getCoinCode($coin_html);
 
 $url = GET_COIN_URL.$coin_code;
-echo send($url);
+$info_html = send($url);
+if(preg_match("/每日登录奖励已领取/", $info_html)){
+	log("success!");
+}else{
+	log("false!");
+}
